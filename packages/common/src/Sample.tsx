11a9e0bbd40fcccd139b11f77fda3e76e0f1d0ee
sakira <yohachi.suga@gmail.com>
2020-01-25 15:10:31 +0900

    add reference from front to common

---
+++ packages/common/src/Sample.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+type Props= {};
+type State = {};
+export default class Sample extends React.Component<Props,State>{
+    constructor(props:Props){
+        super(props)
+    }
+
+    render():React.ReactElement{
+        return(
+            <div>test</div>
+        );
+    }
+}
--- packages/front/src/App.js
+++ packages/front/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sample from "../../common/src/Sample";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Sample></Sample>
       </header>
     </div>
   );
